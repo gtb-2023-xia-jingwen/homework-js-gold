303688c975decfdad868b3d52cd1fc0de556293a
Li <lzhou@thoughtworks.com>
2022-05-13 16:25:00 +0800

    chore: remove api.js

--- src/api.js
+++
@@ -1,9 +0,0 @@
-async function fetchProducts(url) {
-  // fetch products data from here
-}
-
-async function deleteProduct(url, id) {
-  // delete product from here
-}
-
-export { fetchProducts, deleteProduct };
