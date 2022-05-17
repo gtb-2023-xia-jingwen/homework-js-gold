aceb82e03e2a41a491621be4cfdc4cd75eb508de
Li <lzhou@thoughtworks.com>
2022-05-17 10:49:49 +0800

    add product img and other info

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // 从json-server 获取数据
 // 根据数据修改页面
+import placeholderImg from './assets/placeholder-image.png';
 
 function fetchProducts() {
   return fetch('http://localhost:3000/products').then((res) => res.json());
@@ -7,7 +8,11 @@ function fetchProducts() {
 
 function renderProducts(products) {
   const htmlList = products.map((product) => {
-    return `<li>${product.name}</li>`;
+    return `<li>
+                <img src="${placeholderImg}" alt="placeholderImg">
+                <h2>${product.name}</h2>
+                <p>价格：${product.price} / ${product.unit}</p>
+            </li>`;
   });
 
   document.querySelector('.products').innerHTML = htmlList.join('');
