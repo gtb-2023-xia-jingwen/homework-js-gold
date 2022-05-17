511150534d0b8dca1bf5ba6f3ea041d7fc867a77
Li <lzhou@thoughtworks.com>
2022-05-17 12:36:20 +0800

    delete product

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ function fetchProducts() {
 
 function renderProducts(products) {
   const htmlList = products.map((product) => {
-    return `<li>
+    return `<li id="product${product.id}">
                 <img src="${placeholderImg}" alt="placeholderImg">
                 <h2>${product.name}</h2>
                 <p>价格：${product.price} / ${product.unit}</p>
+                <button class="delete-btn" data-id="${product.id}">删除</button>
             </li>`;
   });
 
@@ -20,4 +21,29 @@ function renderProducts(products) {
 
 fetchProducts().then((data) => {
   renderProducts(data);
+});
+
+// 删除商品
+// 1. 添加删除按钮
+// 2. 给删除按钮绑定click事件处理函数
+// 3. 声明事件处理函数：
+//    1. 向 json-server 发送 delete request
+//    2. 删除页面上的商品元素
+
+const deleteProduct = (id) =>
+  fetch(`http://localhost:3000/products/${id}`, {
+    method: 'DELETE',
+  }).then((res) => res.json());
+
+function removeProductItem(id) {
+  document.querySelector(`#product${id}`).remove();
+}
+
+document.addEventListener('click', (event) => {
+  if (event.target.matches('.delete-btn')) {
+    const { id } = event.target.dataset;
+    deleteProduct(id).then(() => {
+      removeProductItem(id);
+    });
+  }
 });
